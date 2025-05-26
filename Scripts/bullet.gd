11e9895f1717b8b0cfc3e11009edfd151545dd05
dginovker <danielginovker@gmail.com>
2025-05-25 20:01:34 -0700

    Make bullets stop when they hit the wall

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -9,3 +9,17 @@ func _process(delta):
 	position += direction * speed * delta
 	if position.length() > 5000: # basic lifetime check
 		queue_free()
+		
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body):
+	if body.is_in_group("players"):
+		return  # Don’t collide with players
+
+	if body.is_in_group("walls"):
+		queue_free()  # Todo - come up with something that plays nice with tilemap
+
+	if body.is_in_group("monsters"):
+		# TODO: damage logic here
+		queue_free()
