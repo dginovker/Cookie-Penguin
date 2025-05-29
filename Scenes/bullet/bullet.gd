acb422b87e086a133e42e01417c1df8ff5ed44b1
dginovker <danielginovker@gmail.com>
2025-05-28 18:00:32 -0700

    Only make server try to despawn bullets

--- Scenes/bullet/bullet.gd
+++ Scenes/bullet/bullet.gd
@@ -8,12 +8,18 @@ func _process(delta):
 		return
 	position += direction * speed * delta
 	if position.length() > 5000: # basic lifetime check
-		queue_free()
+		if multiplayer.is_server():
+			queue_free()
 		
 func _ready():
+	if not multiplayer.is_server():
+		return
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
+	if not multiplayer.is_server():
+		return
+		
 	if body.is_in_group("players"):
 		return  # Don’t collide with players
 
