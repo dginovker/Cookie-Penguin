b7242cb0e299adb8014b1700c295edc4b159b647
dginovker <danielginovker@gmail.com>
2025-08-09 22:54:07 -0700

    Fix enemies and lootbags not being visible on Andorid due to gpu compression support

--- Scenes/serverselect/ServerConnection.gd
+++ Scenes/serverselect/ServerConnection.gd
@@ -8,8 +8,8 @@ func _ready():
     if OS.get_name() == "Web" || OS.has_feature("client"):
         # Join the server
         print("Joining as Websocket Client..")
-        peer.create_client("ws://127.0.0.1:10000") # Test Local!!
-        #peer.create_client("wss://duck.openredsoftware.com/pinkdragon") # Real server!asdw        
+        #peer.create_client("ws://127.0.0.1:10000") # Test Local!!
+        peer.create_client("wss://duck.openredsoftware.com/pinkdragon") # Real server!asdw        
         multiplayer.multiplayer_peer = peer
     else:
         # We are the server
