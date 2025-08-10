168e85a217cf5cfa367fcd89fe398dae8490b7eb
dginovker <danielginovker@gmail.com>
2025-08-09 19:48:13 -0700

    Make bullets look better

--- Scenes/items/WeaponHelper.gd
+++ Scenes/items/WeaponHelper.gd
@@ -3,14 +3,14 @@ extends Node
 func get_cooldown(player_id: int) -> float:
     var weapon: ItemInstance = ItemManager.get_item_at(ItemLocation.new(ItemLocation.Type.PLAYER_GEAR, player_id, 0))
     if weapon == null:
-        return 0.2
+        return 0.4
             
     if weapon.item_type == 'tier_0_sword':
-        return 0.2
+        return 0.4
     elif weapon.item_type == 'tier_1_sword':
-        return 0.18
+        return 0.35
     elif weapon.item_type == 'tier_2_sword':
-        return 0.16
+        return 0.3
     
     assert(false, "I don't know the cooldown for " + weapon.item_type)
     return 1
