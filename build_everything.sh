0d935b8c453fa48601d1b09f66d395beca62a254
dginovker <danielginovker@gmail.com>
2025-08-21 19:55:09 -0700

    Fix some multiplayer sync bugs

--- build_everything.sh
+++ build_everything.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+set -e  # Exit immediately if any command fails
+
 # Build the server
-godot --headless --export-debug "Linux"
+godot --headless --export-debug "Linux" 
 
 # Build the webclient
-rm build.zip
-rm build/index.html ; godot --headless --export-debug "Web" ./build/index.html ; 
+rm -f build.zip
+rm -f build/index.html 
+
+godot --headless --export-debug "Web" ./build/index.html
 
 # Package the build for itch.io
 zip -r build.zip build 
