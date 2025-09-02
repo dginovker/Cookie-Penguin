579512a46c37774c99624cc252dd967899313495
dginovker <danielginovker@gmail.com>
2025-09-01 20:44:35 -0700

    ok set mob pos update back to unreliable ordered, it looks like in the default configuration it blocks for 1m while waiting for an older udp packet

--- Globals/Net.gd
+++ Globals/Net.gd
@@ -16,7 +16,7 @@ var is_client: bool = false
 # called with create_client and create_server
 const SNAPSHOT_CHANNEL = 1
 const SPAWN_CHANNEL = 2
-const ADDITIONAL_CHANNELS = [MultiplayerPeer.TransferMode.TRANSFER_MODE_UNRELIABLE, MultiplayerPeer.TransferMode.TRANSFER_MODE_RELIABLE]
+const ADDITIONAL_CHANNELS = [MultiplayerPeer.TransferMode.TRANSFER_MODE_UNRELIABLE_ORDERED, MultiplayerPeer.TransferMode.TRANSFER_MODE_RELIABLE]
 
 func start_server() -> void:
     is_client = false
